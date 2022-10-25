8c93d6ae1e59104a686e8a059edf1f2103dab04b
sudharaD <sudharad7@gmail.com>
2022-10-25 22:51:38 +0530

    data types - string, integer, float & boolean

---
+++ data_types.py
@@ -0,0 +1,18 @@
+print(len("HEllo"))
+
+print("Hello"[0])
+
+print("123" + "345")
+
+print(123 + 345)
+
+# Integers - Negative & Positive both
+# can seperate numbers like thin in python
+print(1_2 + 3_8)
+
+# Float
+123.456
+
+# Boolear
+True
+False
