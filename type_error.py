4b2a8788b7850e1622a9948fb326ac5ccf51ef84
sudharaD <sudharad7@gmail.com>
2022-10-27 23:10:53 +0530

    type errors

---
+++ type_error.py
@@ -0,0 +1,17 @@
+name_char = len(input("What is your name? "))
+
+name_char_to_string = str(name_char)
+
+print("Your namr has", name_char, "characters")
+
+print("Your namr has " + name_char_to_string + " characters")
+
+print("Your namr has "  + str(name_char) + " characters")
+
+num_1 = str(name_char)
+
+print(type(num_1))
+
+print(70 + float("100.5"))
+
+print(type(70 + float("100.5")))
