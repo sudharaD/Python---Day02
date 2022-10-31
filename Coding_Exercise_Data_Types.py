78b876361b5207c88a1ed94f4e2f749758a17be2
sudharaD <sudharad7@gmail.com>
2022-10-31 23:30:38 +0530

    codeing challenge - data types

---
+++ Coding_Exercise_Data_Types.py
@@ -0,0 +1,20 @@
+# Coding_Exercise_Data_Types
+
+while True:
+
+    try:
+
+        number = input("Enter two digit number: ")
+
+        first_character = int(number[0])
+        second__character = int(number[1])
+
+        print(first_character + second__character)
+
+        break
+
+    except:
+        print("Please enter a two digit number!")
+
+
+
